224a575a2286cdfb4b755d569a26654a7bd0adea
Kyle Riley <kmr145@miami.edu>
2021-08-02 17:43:21 -0400

    first commit

---
+++ project1.py
@@ -0,0 +1,16 @@
+# Present a menu to users that says the following:
+# Enter "about" to learn more about me
+# Enter "q&a" to ask me a question
+# Enter "guestbook" to get added to my guestbook
+# Enter "random" for something random
+# Enter "goodbye" to say goodbye!
+
+print("")
+
+# Each of those inputs should trigger a coordinating function:
+# about_me() - returns a multi-line string with a short bio about yourself
+# q_and_a() - reurns a random answer from a preset list of options you choose 
+# guestbook() - ask the user for their firstname,lastname, and a message and it saves it to a dictionary
+# random() - returns
+
+# menu stays active until user inputs "goodbye"
